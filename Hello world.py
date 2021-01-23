37fdb7b05f4561e00d3eb38d5e199169b03ed561
ArvisV <76994493+ArvisV@users.noreply.github.com>
2021-01-23 18:16:25 +0200

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,10 @@
+# Šī programm sasveicinas un jautā tavu vārdu
+
+print("Hello world")
+print("What is your name?") # Jautā tavu vārdu programma
+myName = input()
+print("It is good to meet you, " + myName)
+print("The leght of your name is: " + str(len(myName)))
+print("What is your age?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
